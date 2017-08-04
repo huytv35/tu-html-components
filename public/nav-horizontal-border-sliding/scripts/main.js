0d7bdb9cbc73bbdbee13776bd114126dfd897e4c
Huy Truong Van <huytv35@gmail.com>
2017-08-04 16:19:33 +0700

    udpate components nav horizontal-border-sliding

---
+++ public/nav-horizontal-border-sliding/scripts/main.js
@@ -0,0 +1,66 @@
+jQuery(document).ready(function($){
+    var item = $('.nav-hor__item');
+
+    function get_left(items, current) {
+        var width = 0;
+        for (var i = 0; i < (current.index() - 1); i++) {
+             width += items[i] + 44;
+        }
+        return width;
+    }
+
+    function get_items_width(el){
+        var array = [];
+        el.each(function(){
+            array.push($(this).width());
+        });
+
+        return array;
+    }
+
+    var items_width = get_items_width(item);
+    item.removeClass('is--active2');
+
+    setTimeout(function(){
+        var border = $('.nav-hor__border');
+        var item_active = $('.nav-hor__item.is--active');
+        var item_not_active = item.not('.is--active');
+        var item_active_left = 0;
+
+        var items_new_width = get_items_width(item);
+
+        if (item_active.index() !== 1) {
+            item_active_left = get_left(items_new_width, item_active);
+        }
+
+        border.css({
+            'left' : item_active_left,
+            'width': items_width[item_active.index() - 1]
+        });
+
+        item_not_active.hover(function(event){
+            var _this = $(this);
+            var _this_index = $(this).index();
+            var current_left = 0;
+
+            item_not_active.removeClass('is--active2');
+            $(this).addClass('is--active2');
+
+            current_left = get_left(items_new_width, _this);
+
+            border.css({
+                'left' : current_left,
+                'width': items_width[_this_index - 1]
+            });
+        }, function(){
+            item_not_active.removeClass('is--active2');
+
+            border.css({
+                'left' : item_active_left,
+                'width': item_active.width()
+            });
+        });
+
+        $('.nav-hor--borders-sliding').addClass('is--visible');
+    }, 800);
+});
